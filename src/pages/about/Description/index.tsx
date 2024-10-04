f03ba35d50c5e69765c68e2c24a7dbb08b840daa
Cristian da Rosa Melo <cristian_melo@sicredi.com.br>
2024-10-04 05:37:00 -0300

    📱 refactor: adjusted about responsiveness

--- src/pages/about/Description/index.tsx
+++ src/pages/about/Description/index.tsx
@@ -6,18 +6,20 @@ export const Description: React.FC = () => {
   const { isDarkMode } = useTheme();
 
   return (
-    <div className={`flex flex-row items-center ${isDarkMode ? 'bg-gray-800' : 'bg-gray-100'} p-8 mt-10 rounded-lg shadow-sm md:flex-row md:justify-center md:p-12`}>
+    <div
+      className={`flex flex-col md:flex-row items-center md:items-start ${isDarkMode ? 'bg-gray-800' : 'bg-gray-100'} p-8 mt-10 rounded-lg shadow-sm md:justify-center md:p-12`}
+    >
       <img
         src={avatarImg}
         alt="Avatar"
-        className="w-48 h-48 rounded-full object-cover"
+        className="w-48 h-48 rounded-full object-cover mb-4 md:mb-0 md:mr-6"
       />
-      <div className="flex flex-col mt-4 md:mt-0 md:ml-6">
+      <div className="flex flex-col flex-grow">
         <h2 className={`text-[30px] font-medium text-center md:text-left ${isDarkMode ? 'text-white' : 'text-black'}`}>Cristian Melo</h2>
         <p className={`text-[14px] font-extralight text-center md:text-left ${isDarkMode ? 'text-gray-300' : 'text-black'}`}>
           I am a Front-End developer who enjoys creating impactful experiences for users. With expertise in React and interface design, I strive to develop robust applications that prioritize usability and efficiency. I have a keen eye for detail and empathy for the user. My experience includes projects that promote innovative solutions. Today, I work on projects worldwide, coding from my home office in the metropolitan area of Porto Alegre, Brazil.
         </p>
-        <div className="mt-4 flex flex-wrap space-x-2">
+        <div className="mt-4 flex flex-wrap gap-2"> {/* Alterado aqui */}
           <Badge text="React" />
           <Badge text="JavaScript" />
           <Badge text="Tailwind CSS" />
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,14 +1,14 @@
-import { HeaderTitle } from "../../components/HeaderTitle"
-import { Description } from "./Description"
+import { HeaderTitle } from "../../components/HeaderTitle";
+import { Description } from "./Description";
 
 export const About = () => {
   return (
     <section className="flex flex-col min-h-screen ">
       <div className="flex-grow flex flex-col items-start justify-start pt-12">
-        <HeaderTitle level="h2" children='about me' />
+        <HeaderTitle level="h2" children='About Me' />
         <Description />
       </div>
       <div className="h-px bg-custom-gray w-full" />
     </section>
-  )
-}
+  );
+};
