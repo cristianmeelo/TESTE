28dbbbe67c16a2f2bea7d6f6154c6706b1b837c9
Cristian da Rosa Melo <cristian_melo@sicredi.com.br>
2024-10-04 03:06:37 -0300

    ✨ feat: update home page with theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { useTheme } from "./context/theme"
 import { About } from "./pages/about"
 import { Contact } from "./pages/contact"
 import { Home } from "./pages/home"
 import { Projects } from "./pages/projects"
 
 export const App = () => {
+  const { isDarkMode } = useTheme();
 
   return (
-    <>
+    <main className={`px-12 ${isDarkMode ? 'bg-dark-bg' : 'bg-light-bg '}`}>
       <Home />
       <Projects />
       <About />
       <Contact />
-    </>
+    </main>
   )
 }
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,23 @@
+import { Header } from "../../components/header";
+import { Button } from "../../components/button";
+import { useTheme } from "../../context/theme";
+
 export const Home = () => {
+  const { toggleDarkMode, isDarkMode } = useTheme();
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="h-1 bg-black w-full" />
-      <div className="flex-grow flex items-center justify-center">
+    <div className={`flex flex-col min-h-screen ${isDarkMode ? 'bg-dark-bg text-dark-text' : 'bg-light-bg text-light-text'}`}>
+      <div className={'absolute left-0 right-0 top-0 h-1 bg-dark-bg'} />
+      <Header />
+      <div className="flex-grow flex items-center justify-center bg-red-700 mb-10">
         <h1 className="text-center">Home</h1>
       </div>
       <div className="h-px bg-custom-gray w-full" />
+      <div className="fixed bottom-4 right-4 transform -rotate-90 -translate-y-12">
+        <Button onClick={toggleDarkMode} ariaLabel="Toggle dark mode">
+          Dark Mode.
+        </Button>
+      </div>
     </div>
   );
-}
+};
